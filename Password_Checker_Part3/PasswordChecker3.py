914ec2669db1e29dbc624c240c1d00f3f60bfbad
Michael KOSTERIADIS (001244342) <Michael.KOSTERIADIS@student.tafesa.edu.au>
2025-11-21 10:42:26 +1030

    changed psuedocode to match

--- Password_Checker_Part3/PasswordChecker3.py
+++ Password_Checker_Part3/PasswordChecker3.py
@@ -18,6 +18,8 @@ PasswordChecker3
 #           count_pw_too_short.APPEND data
 #       ELSE:
 #           count_pw_too_long.APPEND data
+#   FOR line in list_data
+#       PRINT line
 #   PRINT "Number of passwords too short: {} \n Number of passwords too long: {}".FORMAT LEN count pw too short, LEN count pw too long
 
 def main():
